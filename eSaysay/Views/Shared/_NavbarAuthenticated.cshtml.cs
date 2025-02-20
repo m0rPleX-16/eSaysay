387c74a3c9c7e9bdfe377a6703a10bc6fe4d5f18
einna <einnajoyc003@gmail.com>
2025-02-20 23:38:56 +0800

    update exercise. separated navbars.

---
+++ eSaysay/Views/Shared/_NavbarAuthenticated.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eSaysay.Views.Shared
+{
+    public class _NavbarAuthenticatedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
