bf55ca8ad779fa1e8f837328f2625790521aa625
Melquiades Andrade <yunissue16@gmail.com>
2025-02-23 11:28:24 +0800

    Added the files for Admin

--- eSaysay/Controllers/AdminController.cs
+++ eSaysay/Controllers/AdminController.cs
@@ -18,6 +18,36 @@ namespace eSaysay.Controllers
         {
             return View("~/Views/User/Admin/Index.cshtml");
         }
+        public IActionResult Analytics()
+        {
+            return View("~/Views/User/Admin/Analytics.cshtml");
+        }
+        public IActionResult Exercises()
+        {
+            return View("~/Views/User/Admin/Exercises.cshtml");
+        }
+        public IActionResult Lessons()
+        {
+            return View("~/Views/User/Admin/Lessons.cshtml");
+        }
+        public IActionResult Logs()
+        {
+            return View("~/Views/User/Admin/Logs.cshtml");
+        }
+        public IActionResult Progress()
+        {
+            return View("~/Views/User/Admin/Progress.cshtml");
+        }
+        public IActionResult Students()
+        {
+            return View("~/Views/User/Admin/Students.cshtml");
+        }
+
+        public IActionResult Settings()
+        {
+            return View("~/Views/User/Admin/Settings.cshtml");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ eSaysay/Views/User/Admin/Exercises.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace eSaysay.Views.User.Admin
+{
+    public class ExercisesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
