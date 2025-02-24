155c1a84a6147a776628f27745af112d04e3b893
einna <einnajoyc003@gmail.com>
2025-02-24 15:27:30 +0800

    edited the projecttt

--- eSaysay/wwwroot/assets/js/script.js
+++ eSaysay/wwwroot/assets/js/script.js
@@ -1,13 +1,40 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
     const toggler = document.querySelector('.sidebar-toggler');
     const sidebar = document.querySelector('.sidebar');
+    const menuToggler = document.querySelector('.menu-toggler');
     const mainContent = document.querySelector('.main-content');
     const footer = document.querySelector('.footer');
 
+    const collapsedSidebarHeight = "56px";
+    const fullSidebarHeight = "calc(100vh - 124px)";
+
+    // Toggle sidebar's collapsed state
     toggler.addEventListener('click', function () {
         sidebar.classList.toggle('collapsed');
         mainContent.classList.toggle('collapsed');
         footer.classList.toggle('collapsed');
+    });
+
+    // Update sidebar height and menu toggle text
+    const toggleMenu = (isMenuActive) => {
+        sidebar.style.height = isMenuActive ? `${sidebar.scrollHeight}px` : collapsedSidebarHeight;
+        menuToggler.querySelector("span").innerText = isMenuActive ? "close" : "menu";
+    };
+
+    // Toggle menu active class and adjust height
+    menuToggler.addEventListener("click", () => {
+        toggleMenu(sidebar.classList.toggle("menu-active"));
+    });
+
+    // Adjusting sidebar height on window resize
+    window.addEventListener("resize", () => {
+        if (window.innerWidth >= 1024) {
+            sidebar.style.height = fullSidebarHeight;
+        } else {
+            sidebar.classList.remove("collapsed");
+            sidebar.style.height = "auto";
+            toggleMenu(sidebar.classList.contains("menu-active"));
+        }
     });
 
     const carousels = document.querySelectorAll('.carousel');
