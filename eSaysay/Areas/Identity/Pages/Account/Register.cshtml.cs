33f08ba895cf9377313b04e89bda6410eb23c07f
Melquiades Andrade <yunissue16@gmail.com>
2025-03-10 02:47:13 +0800

    Updated password limits

--- eSaysay/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ eSaysay/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -122,7 +122,7 @@ using System.Text.Json;
                 ///     directly from your code. This API may change or be removed in future releases.
                 /// </summary>
                 [Required]
-                [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+                [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 12)]
                 [DataType(DataType.Password)]
                 [Display(Name = "Password")]
                 public string Password { get; set; }
